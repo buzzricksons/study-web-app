6fb1c3585b93afa59ad7cb2d680a1ca48cab525f
HyungCheol Kim <hcgoon@gmail.com>
2018-03-08 19:08:29 +0900

    add thymeleaf html

--- src/main/java/com/example/exam5/Exam5RestController.java
+++ src/main/java/com/example/exam5/Exam5RestController.java
@@ -1,7 +1,5 @@
 package com.example.exam5;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
